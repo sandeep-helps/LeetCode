9cde582b826b96ef1c2e9c330ac1d8ac642aaaf6
Sandeep Sharma <175778127+sandeep-helps@users.noreply.github.com>
2026-06-22 11:24:33 +0530

    Time: 2 ms (19.9%), Space: 8.8 MB (84.03%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int b = 0;
+        int a = 0;
+        int l = 0;
+        int o = 0;
+        int n = 0;
+        for(char ch : text){
+            if(ch == 'b'){
+                b++;
+            }
+            else if(ch == 'a'){
+                a++;
+            }
+            else if(ch == 'l'){
+                l++;
+            }
+            else if(ch == 'o'){
+                o++;
+            }
+            else if(ch == 'n'){
+                n++;
+            }
+            
+        }
+        return min({b,a,( l /2), (o/2), n});
+    }
+
+};
