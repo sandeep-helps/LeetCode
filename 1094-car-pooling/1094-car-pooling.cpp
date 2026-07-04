a52de3c808e2d7d61989a0b8a269a88b2d25f069
Sandeep Sharma <175778127+sandeep-helps@users.noreply.github.com>
2026-07-04 06:48:02 +0530

    Time: 14 ms (5.45%), Space: 14.6 MB (29.13%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -3,21 +3,20 @@ public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         map<int,int>events;
         for(auto &t : trips){
-            int count = t[0];
-            int st = t[1];
+            int passenger = t[0];
+            int start = t[1]; 
             int end = t[2];
 
-            events[st] += count;
-            events[end] -= count;
+            events[start] += passenger;
+            events[end] -= passenger;
         }
         int passcount = 0;
-        vector<int>ans;
-        for(auto & it : events){
+        for(auto &it : events){
             passcount += it.second;
             if(passcount>capacity){
                 return false;
             }
-        }
-        return true; 
+        } 
+        return true;  
     }
 };
