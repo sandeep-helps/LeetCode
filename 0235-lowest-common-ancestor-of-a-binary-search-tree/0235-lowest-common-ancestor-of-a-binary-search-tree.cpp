e940717b456638ff9a22df9752fede49fc5c5c55
Sandeep Sharma <175778127+sandeep-helps@users.noreply.github.com>
2026-09-06 22:29:46 +0530

    Time: 24 ms (49.14%), Space: 23.6 MB (35.63%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,14 +14,12 @@ public:
         if(!root){
             return NULL;
         }
-        if(root->val > p->val && root->val > q->val){
-            return lowestCommonAncestor(root->left, p , q);
+        if(p->val < root->val && q->val < root->val){
+            return lowestCommonAncestor(root->left , p , q);
         }
-        else if(root->val < p->val && root->val < q->val){
-            return lowestCommonAncestor(root->right, p, q);
+        if(p->val > root->val && q->val > root->val){
+            return lowestCommonAncestor(root->right , p , q);
         }
-        else{
-            return root;
-        }
+        return root;
     }
 };
